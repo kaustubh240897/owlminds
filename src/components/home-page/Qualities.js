927ffa2774b9fabd501a62ecbad9132a65faf745
kaustubh2408 <k.dwivedi@chai.finance>
2025-03-20 00:49:07 +0530

    Added a fix for slider

--- src/components/home-page/Qualities.js
+++ src/components/home-page/Qualities.js
@@ -15,7 +15,7 @@ export default function Qualities() {
       </div>
 
       {/* Card grid container */}
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full max-w-6xl mx-auto p-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full max-w-6xl mx-auto p-4 md:gap-y-0">
         {/* Cards */}
         <div className="bg-[url(@/assets/images/features_card4.png)] bg-cover bg-center h-80 rounded-xl shadow-lg">
           <div className="bg-gradient-to-r from-[#3F61E9] to-[#80C4E9] bg-clip-text text-transparent text-2xl md:text-3xl font-bold mt-16 md:mt-20 ml-5 md:ml-10">
@@ -25,7 +25,7 @@ export default function Qualities() {
             </div>
           </div>
         </div>
-        <div className="bg-[url(@/assets/images/features_card3.png)] bg-cover bg-center h-80 rounded-xl shadow-lg">
+        <div className="bg-[url(@/assets/images/features_card3.png)] bg-cover bg-center h-80 rounded-xl shadow-lg md:mt-10">
           <div className="bg-gradient-to-r from-[#3F61E9] to-[#80C4E9] bg-clip-text text-transparent text-2xl md:text-3xl font-bold mt-16 md:mt-20 ml-5 md:ml-10">
             AI & Metaverse Explorers
             <div className="text-[#757575] text-sm md:text-base font-semibold mt-3 md:mt-4 max-w-[250px] md:max-w-[300px]">
@@ -35,13 +35,13 @@ export default function Qualities() {
         </div>
         <div className="bg-[url(@/assets/images/features_card1.png)] bg-cover bg-center h-80 rounded-xl shadow-lg">
           <div className="bg-gradient-to-r from-[#3F61E9] to-[#80C4E9] bg-clip-text text-transparent text-2xl md:text-3xl font-bold mt-16 md:mt-20 ml-5 md:ml-10">
-            Building Financial Confidence
+            Building <br/> Financial Confidence
             <div className="text-[#757575] text-sm md:text-base font-semibold mt-3 md:mt-4 max-w-[250px] md:max-w-[300px]">
               Cultivate a sense of responsibility and financial confidence through practical lessons in money management.
             </div>
           </div>
         </div>
-        <div className="bg-[url(@/assets/images/features_card2.png)] bg-cover bg-center h-80 rounded-xl shadow-lg">
+        <div className="bg-[url(@/assets/images/features_card2.png)] bg-cover bg-center h-80 rounded-xl shadow-lg md:mt-10">
           <div className="bg-gradient-to-r from-[#3F61E9] to-[#80C4E9] bg-clip-text text-transparent text-2xl md:text-3xl font-bold mt-16 md:mt-20 ml-5 md:ml-10">
             Entrepreneurship
             <div className="text-[#757575] text-sm md:text-base font-semibold mt-3 md:mt-4 max-w-[250px] md:max-w-[300px]">
