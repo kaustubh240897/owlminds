ade83c8933ba84be24bf4ae098cd03f9936fcf95
kaustubh2408 <k.dwivedi@chai.finance>
2025-04-01 15:34:05 +0530

    Added meta fb

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import "react-phone-number-input/style.css";
 import { Baloo_2 } from "next/font/google";
+import Script from "next/script"; // Import Next.js Script component
 import "../styles/globals.css";
 
 const baloo2 = Baloo_2({
@@ -10,8 +11,39 @@ const baloo2 = Baloo_2({
 
 export default function App({ Component, pageProps }) {
   return (
-    <main className={baloo2.className}>
-      <Component {...pageProps} />
-    </main>
+    <>
+      {/* Meta Pixel Script */}
+      <Script
+        strategy="afterInteractive" 
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '962895059377944');
+            fbq('track', 'PageView');
+          `,
+        }}
+      />
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src="https://www.facebook.com/tr?id=962895059377944&ev=PageView&noscript=1"
+        />
+      </noscript>
+
+      {/* Main Content */}
+      <main className={baloo2.className}>
+        <Component {...pageProps} />
+      </main>
+    </>
   );
 }
+
